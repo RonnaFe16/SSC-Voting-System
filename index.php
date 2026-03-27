1eb90a895c72f00a0cedafd6ff15359bddb89d5d
ronnafe <ronnafe2@gmail.com>
2026-03-27 16:06:57 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+  	session_start();
+  	if(isset($_SESSION['admin'])){
+    	header('location: admin/home.php');
+  	}
+
+    if(isset($_SESSION['voter'])){
+      header('location: home.php');
+    }
+?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition login-page" style="background-color: lightblue;">
+<div class="login-box">
+  	<div class="login-logo">
+  		<b>SSC Voting System</b>
+  	</div>
+  
+  	<div class="login-box-body text-center mb-2">
+      <img src="images/logo.jpg" width="90px">
+      <h2>Voters Login</h2>
+    	<!-- <p class="login-box-msg"><a href="admin/index.php"> Sign in to start your session</a></p> -->
+
+    	<form action="login.php" method="POST">
+      		<div class="form-group has-feedback">
+        		<input type="text" class="form-control" name="voter" placeholder="Voter's ID" required>
+        		<span class="glyphicon glyphicon-user form-control-feedback"></span>
+      		</div>
+          <div class="form-group has-feedback">
+            <input type="password" class="form-control" name="password" placeholder="Password" required>
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+          </div>
+      		<div class="row">
+      			<div class="col-xs-4">
+          			<button type="submit" class="btn btn-primary btn-block btn-flat" name="login"><i class="fa fa-sign-in"></i> Sign In</button>
+        		</div>
+            <p class="login-box-msg" style="padding-top: 10px;"><a href="admin/index.php"> Login as Admin</a></p>
+      		</div>
+    	</form>
+  	</div>
+  	<?php
+  		if(isset($_SESSION['error'])){
+  			echo "
+  				<div class='callout callout-danger text-center mt20'>
+			  		<p>".$_SESSION['error']."</p> 
+			  	</div>
+  			";
+  			unset($_SESSION['error']);
+  		}
+  	?>
+</div>
+	
+<?php include 'includes/scripts.php' ?>
+</body>
+</html>
